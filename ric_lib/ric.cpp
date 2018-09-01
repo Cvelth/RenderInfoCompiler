5a3b54f0715ab836026d4077baa58a4a7c4e33a4
Cvelth <Cvelth.mail@gmail.com>
2018-09-01 18:25:59 +0300

    version control system was added.

--- ric_lib/ric.cpp
+++ ric_lib/ric.cpp
@@ -1,3 +1,11 @@
+#include "version.hpp"
+#include <sstream>
+std::string ric::get_version() {
+	std::ostringstream s;
+	s << LibraryName << " v" << Version_Major << '.' << Version_Minor << '.' << Version_Patch << '(' << Version_Build << ')';
+	return s.str();
+}
+
 #include "ric.hpp"
 ric::RenderInfoCompiller::RenderInfoCompiller() {}
 ric::RenderInfoCompiller::~RenderInfoCompiller() {}
---
+++ ric_lib/version.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#define LibraryName "RenderInfoCompiler"
+#define Version_Major 0
+#define Version_Minor 0
+#define Version_Patch 0
+#define Version_Build 4
+
+#include <string>
+namespace ric {
+	std::string get_version();
+}
