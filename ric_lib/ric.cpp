ab094d6b2cbf485261320c0b8cc2f209bbd75604
Cvelth <Cvelth.mail@gmail.com>
2018-09-04 07:48:03 +0300

    Object file generation fucntion is now properly called from ric::compile().

--- ric_lib/ric.cpp
+++ ric_lib/ric.cpp
@@ -1,5 +1,6 @@
 #include "version.hpp"
 #include <sstream>
+#include <fstream>
 std::string ric::get_version() {
 	std::ostringstream s;
 	s << LibraryName << " v" << Version_Major << '.' << Version_Minor << '.' << Version_Patch << '(' << Version_Build << ')';
@@ -37,6 +38,7 @@ std::string ric::number(double const& s) {
 namespace ric {
 	std::list<Token> tokenize(std::string const& path);
 	Tree analyze(std::list<Token> const& tokens);
+	void generate(std::ofstream &f, Tree tree);
 }
 void ric::compile(std::string const& path) {
 	std::list<ric::Token> tokens;
@@ -44,6 +46,14 @@ void ric::compile(std::string const& path) {
 	try {
 		tokens = tokenize(path);
 		tree = analyze(tokens);
+		std::ofstream obj_file;
+		auto pos = path.find_last_of('.');
+		if (pos == path.size())
+			throw Exceptions::CompilationError("Error determining object file name. Make sure source file isn't corrupted and is prprly named.", 0, 0, path);
+		obj_file.open(path.substr(0, pos) + ".rio", std::ios::binary | std::ios::out);
+		if (!obj_file)
+			throw Exceptions::CompilationError("Unable to open object file for writing. It's possibly already in use.", 0, 0, path);
+		generate(obj_file, tree);
 	} catch (Exceptions::InnerCompilationError &e) {
 		throw Exceptions::CompilationError(e, path);
 	}
