aabe86a5a0cda5ef4fd805d288224aa98103b69b
cvelth <cvelth.mail@gmail.com>
2018-09-12 22:42:15 +0300

    Primitive library was implemented.

--- ric_lib/Library.cpp
+++ ric_lib/Library.cpp
@@ -1,12 +1,50 @@
 #include "shared.hpp"
+#include <cmath>
+#define M_PI 3.14159265358979323846
+
 ric::Primitive ric::library::ellipse(double aspect_ratio, bool is_filled, size_t points, size_t numbers_per_vertex) {
-	//To be implemeted.
+	Primitive ret(is_filled ? primitive_type::triangle_fan : primitive_type::line_strip, numbers_per_vertex);
+
+	double const step = M_PI / points * 2;
+	if (aspect_ratio < 1.0 && aspect_ratio > 0.0) {
+		for (double f = 0.0; f < M_PI * 2.0; f += step) {
+			ret->push_back(aspect_ratio * cos(f));
+			ret->push_back(sin(f));
+		}
+		ret->push_back(aspect_ratio * cos(0.0));
+		ret->push_back(sin(0.0));
+	} else if (aspect_ratio >= 1.0) {
+		for (double f = 0.0; f < M_PI * 2.0; f += step) {
+			ret->push_back(cos(f));
+			ret->push_back(aspect_ratio * sin(f));
+		}
+		ret->push_back(cos(0.0));
+		ret->push_back(aspect_ratio * sin(0.0));
+	} else 
+		throw Exceptions::LibraryCompilationError("Unsupported aspect_ratio value.");
+
+	return ret;
 }
 ric::Primitive ric::library::circle(bool is_filled, size_t points, size_t numbers_per_vertex) {
 	return ellipse(1.0, is_filled, points, numbers_per_vertex);
 }
 ric::Primitive ric::library::rectangle(double aspect_ratio, bool is_filled, size_t numbers_per_vertex) {
-	//To be implemented.
+	Primitive ret(is_filled ? primitive_type::triangle_strip : primitive_type::line_loop, numbers_per_vertex);
+
+	if (aspect_ratio < 1.0 && aspect_ratio > 0.0) {
+		ret->push_back(-aspect_ratio); ret->push_back(-1.f);
+		ret->push_back(+aspect_ratio); ret->push_back(-1.f);
+		ret->push_back(-aspect_ratio); ret->push_back(+1.f);
+		ret->push_back(+aspect_ratio); ret->push_back(+1.f);
+	} else if (aspect_ratio >= 1.0) {
+		ret->push_back(-1.f); ret->push_back(-1.f / aspect_ratio);
+		ret->push_back(-1.f); ret->push_back(+1.f / aspect_ratio);
+		ret->push_back(+1.f); ret->push_back(-1.f / aspect_ratio);
+		ret->push_back(+1.f); ret->push_back(+1.f / aspect_ratio);
+	} else
+		throw Exceptions::LibraryCompilationError("Unsupported aspect_ratio value.");
+
+	return ret;
 }
 ric::Primitive ric::library::square(bool is_filled, size_t numbers_per_vertex) {
 	return rectangle(1.0, is_filled, numbers_per_vertex);
