7931b53a43a8121308d308c57fc6cd3eb0abd522
cvelth <cvelth.mail@gmail.com>
2019-03-21 18:12:57 +0200

    Extention addition was reworked. ExtentionError was added.

--- ric_ext/extentions.cpp
+++ ric_ext/extentions.cpp
@@ -1,11 +1,11 @@
 #include "extentions.hpp"
-
 #include "primitives.hpp"
-void use_extention::primitives() {
-	ExtentionManager::add(std::make_unique<Primitives>());
-}
-
 #include "transformations.hpp"
-void use_extention::transformations() {
-	ExtentionManager::add(std::make_unique<Transformations>());
+bool ric::ExtentionManager::add(std::string const& name) {
+	if (name == "primitives")
+		return ExtentionManager::add(std::make_unique<Primitives>());
+	else if (name == "transformations")
+		return ExtentionManager::add(std::make_unique<Transformations>());
+	else
+		throw Exceptions::ExtentionError("Unsuported Extention");
 }
--- ric_ext/extentions.hpp
+++ ric_ext/extentions.hpp
@@ -3,20 +3,25 @@
 #include <memory>
 #include <algorithm>
 #include "extention.hpp"
-class ExtentionManager {
-private:
-	inline static std::set<std::unique_ptr<Extention>> enabled_extentions;
-public:
-	static std::set<std::unique_ptr<Extention>> const& get() { return enabled_extentions; }
-	static bool add(std::unique_ptr<Extention> &&ext) {
-		if (std::find_if(enabled_extentions.begin(), enabled_extentions.end(), [&ext](std::unique_ptr<Extention> const& o) {return ext->name() == o->name();}) != enabled_extentions.end())
-			return false;
-		enabled_extentions.insert(std::move(ext));
-		return true;
+namespace ric {
+	namespace Exceptions {
+		class ExtentionError {
+		public:
+			std::string error;
+			ExtentionError(std::string const& error) : error(error) {}
+		};
 	}
-};
-
-namespace use_extention {
-	void primitives();
-	void transformations();
+	class ExtentionManager {
+	private:
+		inline static std::set<std::unique_ptr<Extention>> enabled_extentions;
+	public:
+		static std::set<std::unique_ptr<Extention>> const& get() { return enabled_extentions; }
+		static bool add(std::unique_ptr<Extention> &&ext) {
+			if (std::find_if(enabled_extentions.begin(), enabled_extentions.end(), [&ext](std::unique_ptr<Extention> const& o) {return ext->name() == o->name(); }) != enabled_extentions.end())
+				return false;
+			enabled_extentions.insert(std::move(ext));
+			return true;
+		}
+		static bool add(std::string const& name);
+	};
 }
